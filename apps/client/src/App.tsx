cdf3e52e536951f40eb2c139cd7f075ea4e81288
Ronit Kanoje <ronitkanoje2401@gmail.com>
2026-06-16 14:48:05 +0530

    first message bug solved

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,18 +1,29 @@
 import Otp from "./pages/Otp";
 import Register from "./pages/Register";
 import Chat from "./pages/Chat";
+import Analyst from "./pages/Analyst";
 import OAuthCallback from "./pages/OAuthCallback";
 import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import ProtectedRoute from "./components/ProtectedRoute";
 import { setCredentials, finishLoading } from "./redux/features/authSlice";
+import type { RootState } from "./redux/store";
 import api from "./services/api";
 
 const App = () => {
   const dispatch = useDispatch();
+  const mode = useSelector((state: RootState) => state.theme.mode);
+
+  useEffect(() => {
+    if (mode === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [mode]);
 
   useEffect(() => {
     const refreshAuth = async () => {
@@ -29,7 +40,7 @@ const App = () => {
   }, [dispatch]);
 
   return (
-    <div className="min-h-screen bg-[#f6f8fb] text-[#161b22]">
+    <div className="min-h-screen transition-colors duration-200">
       <Toaster
         position="top-right"
         reverseOrder={false}
@@ -52,6 +63,15 @@ const App = () => {
           element={
             <ProtectedRoute>
               <Chat />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/analyst"
+          element={
+            <ProtectedRoute>
+              <Analyst />
             </ProtectedRoute>
           }
         />
